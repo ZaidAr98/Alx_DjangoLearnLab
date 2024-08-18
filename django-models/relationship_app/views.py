5e7994ca28d00847840fc02d1c31920bdf162df0
Zaid Mohamed <zaidhamid600@gmail.com>
2024-08-18 18:33:11 +0400

    fix 3

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -11,7 +11,7 @@ from .models import Library
 
 def FunctionView(request):
      books = Book.objects.all()
-    return render(request, 'list_books.html', {'books': books})
+    return render(request, 'relationship_app/templates/list_books.html', {'books': books})
 
 
 
