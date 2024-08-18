6cbce971f238e283f259ffb6316d8d4a0b7b3000
Zaid Mohamed <zaidhamid600@gmail.com>
2024-08-18 22:50:08 +0400

    fix 3

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -13,16 +13,10 @@ urlpatterns = [
     path('admin-view/', admin_view, name='admin_view'),
     path('librarian-view/', librarian_view, name='librarian_view'),
     path('member-view/', member_view, name='member_view'),
-    path('books/add/', views.add_book, name='add_book'),
-
-    
-    path('books/edit/<int:pk>/', views.edit_book, name='edit_book'),
-
-   
-    path('books/delete/<int:pk>/', views.delete_book, name='delete_book'),
-
-  
-    path('books/', views.list_books, name='list_books'),
+    path('books/add_book/', views.add_book, name='add_book'),
+    path('books/edit_book/<int:pk>/', views.edit_book, name='edit_book'),
+    path('books/delete_book/<int:pk>/', views.delete_book, name='delete_book'),
+    path('books/', views.list_books, name='list_books'),  # Example for listing books
 
 ]
 
