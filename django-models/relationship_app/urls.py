62583889017cbc980f6b7afd57c0326781123eae
Zaid Mohamed <zaidhamid600@gmail.com>
2024-08-18 18:52:08 +0400

    fix11

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views import ClassView, FunctionView
-from .views import "list_books", "LibraryDetailView"
+from .views import list_books, LibraryDetailView
+
 urlpatterns = [
     path('books/', ClassView, name='list_books'), 
     path('library/<int:pk>/',FunctionView.as_view(), name='LibraryDetailView'), 
