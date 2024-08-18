aeb2712f109add96ae12b5e96b8b29ee168a6b1c
Zaid Mohamed <zaidhamid600@gmail.com>
2024-08-18 21:59:15 +0400

    fix 10

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -31,5 +31,5 @@ class LibraryDetailView(DetailView):
 class register(CreateView):
     model = User
     template_name = 'relationship_app/templates/relationship_app/register.html'
-    form_class = UserCreationForm
+    form_class = UserCreationForm()
     success_url = reverse_lazy('login')
