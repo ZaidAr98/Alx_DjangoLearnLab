bc08b5d0a5c0b12e1913d5cf9f93bf7ba11e9cee
Zaid Mohamed <zaidhamid600@gmail.com>
2024-08-18 20:19:08 +0400

    fix 1

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 
-from .views import list_books, LibraryDetailView
+from .views import list_books, LibraryDetailView,login_view, RegisterView, CustomLogoutView
 
 urlpatterns = [
     path('books/', ClassView, name='list_books'), 
     path('library/<int:pk>/',FunctionView.as_view(), name='LibraryDetailView'), 
+     path('login/', login_view, name='login'),
+    path('logout/', CustomLogoutView.as_view(), name='logout'),
+    path('register/', RegisterView.as_view(), name='register'),
 ]
--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 from .models import Book
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView
+from django.contrib.auth.views import LogoutView
+
 
 def list_books(request):
     books = Book.objects.all()  
@@ -12,3 +19,25 @@ class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/temlates/relationship_app/library_detail.html' 
     context_object_name = 'library'
+
+
+
+# Registration view using Django's built-in form
+class RegisterView(CreateView):
+    form_class = UserCreationForm
+    template_name = 'relationship_app/temlates/relationship_app/register.html'
+    success_url = reverse_lazy('login')
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('home')  # Redirect to a home page or any other page
+    else:
+        form = AuthenticationForm()
+    return render(request, 'relationship_app/temlates/relationship_app/login.html', {'form': form})
+
+class CustomLogoutView(LogoutView):
+    template_name = 'relationship_app/temlates/relationship_app/logout.html'
