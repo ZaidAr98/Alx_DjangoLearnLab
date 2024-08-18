ea6cef14e10648f71c98d479c9191eb033f261d3
Zaid Mohamed <zaidhamid600@gmail.com>
2024-08-18 17:14:40 +0400

    fix 9

--- django-models/relationship_app/query_samples.py
+++ django-models/relationship_app/query_samples.py
@@ -2,7 +2,7 @@ from relationship_app.models import Author, Book, Library, Librarian
 
 # Query all books by a specific author
 author_name = 'Zaid'
-author = Author.objects.filter(name=author_name)
+author = Author.objects.get(name=author_name)
 books_by_author = Book.objects.filter(author=author) # using related_name 'books'
 
 for book in books_by_author:
