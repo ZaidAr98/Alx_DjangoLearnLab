779b0522e2793e6ca5b90a428cc096c7dd62adb1
Zaid Mohamed <zaidhamid600@gmail.com>
2024-08-18 21:50:38 +0400

    fix 6

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -15,27 +15,27 @@ from django.contrib.auth.models import User
 
 def list_books(request):
     books = Book.objects.all()  
-    return render(request, 'relationship_app/temlates/relationship_app/list_books.html', {'books': books})
+    return render(request, 'relationship_app/templates/relationship_app/list_books.html', {'books': books})
 
 from django.views.generic.detail import DetailView
 from .models import Library
 
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = 'relationship_app/temlates/relationship_app/library_detail.html' 
+    template_name = 'relationship_app/templates/relationship_app/library_detail.html' 
     context_object_name = 'library'
 
 
 
 
-class loginView(LoginView):
-    template_name = 'relationship_app/temlates/relationship_app/login.html'
+class CustomLoginView(LoginView):
+    template_name = 'relationship_app/templates/relationship_app/login.html'
 
-class logoutView(LogoutView):
-    template_name = 'relationship_app/temlates/relationship_app/logout.html'
+class CustomLogoutView(LogoutView):
+    template_name = 'relationship_app/templates/relationship_app/logout.html'
 
-class register(CreateView):
-     model = User
-    template_name = 'relationship_app/temlates/relationship_app/register.html'
+class RegisterView(CreateView):
+    model = User
+    template_name = 'relationship_app/templates/relationship_app/register.html'
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
