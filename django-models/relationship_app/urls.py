b22cd764d666fcf12c73e884f98c50949ddc8e3c
Zaid Mohamed <zaidhamid600@gmail.com>
2024-08-18 22:46:06 +0400

    4. Implementing Custom Permissions in Django

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
 from .views import admin_view, librarian_view, member_view
 from .views import list_books, LibraryDetailView
+from . import views
 
 urlpatterns = [
     path('books/', ClassView, name='list_books'), 
@@ -9,9 +10,13 @@ urlpatterns = [
      path('login/', LoginView.as_view(template_name='relationship_app/templates/relationship_app/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='relationship_app/templates/relationship_app/logout.html'), name='logout'),
     path('register/', views.register.as_view(), name='register'),
-       path('admin-view/', admin_view, name='admin_view'),
+    path('admin-view/', admin_view, name='admin_view'),
     path('librarian-view/', librarian_view, name='librarian_view'),
     path('member-view/', member_view, name='member_view'),
+    path('books/add/', views.add_book, name='add_book'),
+    path('books/edit/<int:pk>/', views.edit_book, name='edit_book'),
+    path('books/delete/<int:pk>/', views.delete_book, name='delete_book'),
+    path('books/', views.list_books, name='list_books'),  # Example for listing books
 
 ]
 
--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -11,7 +11,8 @@ from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import permission_required
 
 
 def list_books(request):
@@ -65,3 +66,36 @@ def librarian_view(request):
 @user_passes_test(is_member)
 def member_view(request):
     return render(request, 'relationship_app/templates/relationship_app/member_view.html')
+
+
+
+
+
+@permission_required('relationship_app.can_add_book', raise_exception=True)
+def add_book(request):
+    if request.method == "POST":
+        title = request.POST.get('title')
+        author = request.POST.get('author')
+        published_date = request.POST.get('published_date')
+        Book.objects.create(title=title, author=author, published_date=published_date)
+        return redirect('list_books')
+    return render(request, 'relationship_app/add_book.html')
+
+@permission_required('relationship_app.can_change_book', raise_exception=True)
+def edit_book(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    if request.method == "POST":
+        book.title = request.POST.get('title')
+        book.author = request.POST.get('author')
+        book.published_date = request.POST.get('published_date')
+        book.save()
+        return redirect('list_books')
+    return render(request, 'relationship_app/edit_book.html', {'book': book})
+
+@permission_required('relationship_app.can_delete_book', raise_exception=True)
+def delete_book(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    if request.method == "POST":
+        book.delete()
+        return redirect('list_books')
+    return render(request, 'relationship_app/delete_book.html', {'book': book})
