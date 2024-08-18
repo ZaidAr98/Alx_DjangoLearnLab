76dc989b0e55eb6b1158636b490ff5e7af4df422
Zaid Mohamed <zaidhamid600@gmail.com>
2024-08-18 16:53:09 +0400

    fix 1

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,18 +1,44 @@
-from django.db import models
+# from django.db import models
 
-# Create your models here.
+# # Create your models here.
+
+# class Author(models.Model):
+#     name = models.CharField(max_length=100)
+
+# class Book(models.Model):
+#     name = models.CharField(max_length=100)
+#     author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
+
+# class Library(models.Model):
+#     name = models.CharField(max_length=100)
+#     books = models.ManyToManyField(Book, related_name='libraries')
+
+# class Librarian(models.Model):
+#     name = models.CharField(max_length=100)
+#     library = models.OneToOneField(Library, on_delete=models.CASCADE)
+
+
+
+from django.db import models
 
 class Author(models.Model):
     name = models.CharField(max_length=100)
 
+  
 class Book(models.Model):
-    name = models.CharField(max_length=100)
-    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+
+ 
 
 class Library(models.Model):
     name = models.CharField(max_length=100)
-    books = models.ManyToManyField(Book, related_name='libraries')
+    books = models.ManyToManyField(Book)
+
+  
 
 class Librarian(models.Model):
     name = models.CharField(max_length=100)
     library = models.OneToOneField(Library, on_delete=models.CASCADE)
+
+ 
