961882ab01e39689fca44d66dd63a7dc3bfa6742
Zaid Mohamed <zaidhamid600@gmail.com>
2024-08-18 21:45:12 +0400

    fix 6

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('library/<int:pk>/',FunctionView.as_view(), name='LibraryDetailView'), 
        path('login/', loginView.as_view(template_name='relationship_app/temlates/relationship_app/login.html'), name='login'),
     path('logout/', logoutView.as_view(template_name='relationship_app/temlates/relationship_app/logout.html'), name='logout'),
-    path('register/', register.as_view(), name='relationship_app/temlates/relationship_app/register'),
+    path('register/', register.as_view(), name='register'),
 
 ]
--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -35,7 +35,7 @@ class logoutView(LogoutView):
     template_name = 'relationship_app/temlates/relationship_app/logout.html'
 
 class register(CreateView):
-    model = User
+     model = User
     template_name = 'relationship_app/temlates/relationship_app/register.html'
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
