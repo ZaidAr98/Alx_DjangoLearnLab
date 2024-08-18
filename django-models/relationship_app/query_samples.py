fb33c52a01d50335e4b5b3b19ba6ddf44c943d65
Zaid Mohamed <zaidhamid600@gmail.com>
2024-08-18 17:08:11 +0400

    fix 6

--- django-models/relationship_app/query_samples.py
+++ django-models/relationship_app/query_samples.py
@@ -2,18 +2,21 @@ from relationship_app.models import Author, Book, Library, Librarian
 
 # Query all books by a specific author
 author_name = 'Zaid'
-books_by_author = Book.objects.filter(author=author)
+author = Author.objects.get(name=author_name)
+books_by_author = author.books.all()  # using related_name 'books'
 
-
-
+for book in books_by_author:
+    print(book.name)
 
 # List all books in a library
 library_name = 'Khartoum'
-library = Library.objects.filter(name=library_name)
+library = Library.objects.get(name=library_name)
+books_in_library = library.books.all()  # using related_name 'books'
 
-
+for book in books_in_library:
+    print(book.name)
 
 # Retrieve the librarian for a library
 library_name = 'Mohamed'
-librarian = Librarian.objects.filter(library__name=library_name)
-
+librarian = Librarian.objects.get(library__name=library_name)
+print(f"Librarian of {library_name}: {librarian.name}")
