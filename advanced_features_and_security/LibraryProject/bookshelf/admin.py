31eb8e4fbe5ded119293d471741d0d13b3c74637
Zaid Mohamed <zaidhamid600@gmail.com>
2024-08-25 20:31:55 +0400

    fix 3

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,10 +1,23 @@
 
 # Register your models here.
+# from django.contrib import admin
+# from .models import Book
+
+# class BookAdmin(admin.ModelAdmin):
+#     search_fields = ('title', 'author', 'publication_year')
+#     list_filter = ('title', 'author','publication_year')
+
+# admin.site.register(Book, BookAdmin)
+
+
 from django.contrib import admin
-from .models import Book
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
-class BookAdmin(admin.ModelAdmin):
-    search_fields = ('title', 'author', 'publication_year')
-    list_filter = ('title', 'author','publication_year')
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('date_of_birth', 'profile_photo')}),
+    )
 
-admin.site.register(Book, BookAdmin)
+admin.site.register(CustomUser, CustomUserAdmin)
--- advanced_features_and_security/LibraryProject/relationship_app/admin.py
+++ advanced_features_and_security/LibraryProject/relationship_app/admin.py
@@ -1,18 +1,6 @@
-# from django.contrib import admin
-# from .models import UserProfile
-# # Register your models here.
-# admin.site.register(UserProfile)
-
-
-
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from .models import CustomUser
+from .models import UserProfile
+# Register your models here.
+admin.site.register(UserProfile)
 
-class CustomUserAdmin(UserAdmin):
-    model = CustomUser
-    fieldsets = UserAdmin.fieldsets + (
-        (None, {'fields': ('date_of_birth', 'profile_photo')}),
-    )
 
-admin.site.register(CustomUser, CustomUserAdmin)
