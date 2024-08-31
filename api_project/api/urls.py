e9c28fcc2fcdf54b6aad6851884b725dcaa25c63
Zaid Mohamed <zaidhamid600@gmail.com>
2024-08-31 15:20:34 +0400

    fix 5

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -5,7 +5,7 @@ from rest_framework.authtoken.views import obtain_auth_token
 
 router = DefaultRouter()
 router.register(r'books', BookViewSet)
-
+#urls
 urlpatterns = [
     path('api/', include(router.urls)),
     path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
