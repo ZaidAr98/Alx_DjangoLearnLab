df64d52f58244fa2883ecf9e66e1d2cdfa2ce67a
Zaid Mohamed <zaidhamid600@gmail.com>
2024-09-07 23:54:55 +0400

    fix 4

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
-from rest_framework.permissions import IsAuthenticated,IsAuthenticatedOrReadOnly
-from rest_framework import generics,viewsets
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
+from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
 
