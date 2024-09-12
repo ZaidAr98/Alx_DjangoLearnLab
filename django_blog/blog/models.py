a8232ce3e7711b2b2fc183cfdb99bd7bc706ba03
Zaid Mohamed <zaidhamid600@gmail.com>
2024-09-13 02:02:10 +0400

    fix 2

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,18 +1,16 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 
-class User(models.Model):
-    name = models.CharField(max_length=180)
-
-    def __str__(self):
-        return self.name
+# No need to redefine the User model if you're using Django's built-in User
+# The Blog model can reference the built-in User model
 
 class Blog(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     published_date = models.DateTimeField(auto_now=True)
-    user = models.ForeignKey(User, related_name="blog", on_delete=models.CASCADE)
-  
+    user = models.ForeignKey(User, related_name="blogs", on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
+
