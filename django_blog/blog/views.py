0fb0d340fe9012d5a75511eacc37a597ce21a547
Zaid Mohamed <zaidhamid600@gmail.com>
2024-09-14 13:49:49 +0400

    finish register

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,45 @@
 from django.shortcuts import render
-
+from .forms import CustomUserCreationForm
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import authenticate,login as auth_login
 # Create your views here.
+
+
+
+def home(request):
+    return render(request,'blog/base.html')  
+
+
+from django.contrib import messages
+from django.shortcuts import render, redirect
+from .forms import CustomUserCreationForm
+
+def register(request):
+    form = CustomUserCreationForm()
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Registration successful. You can now log in.")
+            return redirect("login")  # Ensure "home" is a valid URL name in your project
+        else:
+            messages.error(request, "Please correct the error below.")
+    
+    return render(request, "blog/register.html", {'form': form})
+
+
+def user_login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            email = form.cleaned_data.get('email')
+            password = form.cleaned_data.get('password')
+            user = authenticate(request, email=email, password=password)
+            if user is not None:
+                auth_login(request, user)
+                return redirect('home')  # Redirect to your home page
+    else:
+        form = AuthenticationForm()  # Create an empty form
+
+    return render(request, 'blog/login.html', {'form': form})
