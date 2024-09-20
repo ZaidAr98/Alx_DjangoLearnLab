0e3aac43b29446ad5c3377ae6f25689a194ffe36
Zaid Mohamed <zaidhamid600@gmail.com>
2024-09-20 10:36:43 +0400

    1. Implementing the Blog's User Authentication System

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -3,6 +3,8 @@ from .forms import CustomUserCreationForm
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import authenticate,login as auth_login
+from django.contrib.auth import logout as auth_logout
+
 # Create your views here.
 
 
@@ -13,12 +15,12 @@ def home(request):
 
 from django.contrib import messages
 from django.shortcuts import render, redirect
-from .forms import CustomUserCreationForm
+from .forms import UserCreationForm
 
 def register(request):
-    form = CustomUserCreationForm()
+    form = UserCreationForm()
     if request.method == 'POST':
-        form = CustomUserCreationForm(request.POST)
+        form = UserCreationForm(request.POST)
         if form.is_valid():
             form.save()
             messages.success(request, "Registration successful. You can now log in.")
@@ -33,13 +35,41 @@ def user_login(request):
     if request.method == 'POST':
         form = AuthenticationForm(request, data=request.POST)
         if form.is_valid():
-            email = form.cleaned_data.get('email')
+            username = form.cleaned_data.get('username')
             password = form.cleaned_data.get('password')
-            user = authenticate(request, email=email, password=password)
+            user = authenticate(request, username=username, password=password)
             if user is not None:
                 auth_login(request, user)
-                return redirect('home')  # Redirect to your home page
+                return redirect('profile')  # Redirect to your home page
     else:
         form = AuthenticationForm()  # Create an empty form
 
     return render(request, 'blog/login.html', {'form': form})
+
+
+def user_logout(request):
+    auth_logout(request) 
+    return redirect('login')
+
+
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render
+
+@login_required
+def profile(request):
+    return render(request, 'blog/profile.html', {'user': request.user})
+
+
+from django.db.models.signals import post_save
+from django.contrib.auth.models import User
+from django.dispatch import receiver
+from .models import Profile
+
+@receiver(post_save, sender=User)
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_profile(sender, instance, **kwargs):
+    instance.profile.save()
