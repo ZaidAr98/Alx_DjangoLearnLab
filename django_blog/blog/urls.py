c738dd8299a18db807163b358d603695a8b826c2
Zaid Mohamed <zaidhamid600@gmail.com>
2024-09-20 10:39:12 +0400

    fix1

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -4,7 +4,7 @@ from .views import home,register,user_login,user_logout,profile
 
 urlpatterns = [
     path("",home,name="home"),
-    path("register",register,name='register'),
+    path("register/",register,name='register'),
     path('login/',user_login, name='login'),
     path('logout/', user_logout, name='logout'),
     path("profile/",profile,name="profile")
