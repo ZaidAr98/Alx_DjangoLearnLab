45ff40df963b4686e1760d0f422e104207ea2191
Zaid Mohamed <zaidhamid600@gmail.com>
2024-09-28 13:06:08 +0400

    fix 1

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -12,14 +12,13 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
-        user = User(
+        user = User.objects.create_user(
             username=validated_data['username'],
             email=validated_data['email'],
             bio=validated_data.get('bio', ''),
             profile_picture=validated_data.get('profile_picture', None),
         )
-        user.set_password(validated_data['password'])
-        user.save()
+   
 
         # Create a token for the new user
         Token.objects.create(user=user)
